29fcfca56f462de82ebdf4b782f787f195114656
Bilal Saeed <rdev56573@gmail.com>
2023-08-04 14:58:49 +0500

    Add action edit

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -14,9 +14,22 @@ class ChefsController < ApplicationController
   end
 
   def show
-
+    @chef = Chef.find(params[:id])
   end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to @chef
+    else
+      render 'edit', status: 422
+    end
+  end
   private
   def chef_params
     params.require(:chef).permit(:chefname, :email, :password, :password_confirmation)
