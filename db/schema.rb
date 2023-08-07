dd74df8db0ce57754d0a130c852ddc89697ab8e0
Bilal Saeed <rdev56573@gmail.com>
2023-08-07 13:29:23 +0500

    Add ingredient model and M-M association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_143041) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_080458) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_143041) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
