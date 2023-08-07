90d88db41c2f117d0a06ac235c1d25bce2bdfcb5
Bilal Saeed <rdev56573@gmail.com>
2023-08-07 18:04:25 +0500

    Completed comments features

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
+
   #get '/recipes', to: 'recipes#index'
   #get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   #get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   #post '/recipes', to:'recipes#create'
   resources :recipes
-
+  #this is to create route with route id and comments in /15/comments form
+  resources :recipe do
+    resources :comments, only: [:create]
+  end
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
 
